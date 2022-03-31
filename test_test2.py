bc80a5a90f8cd58c362ec01545ef1545fecd0ed3
stefano-sansotta <sansotta.stefano@gmail.com>
2022-03-31 10:51:05 +0200

    Another Test

---
+++ test_test2.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+
+def fizzbuzz(number):
+    return "I don't exist yet"
+
+def New_fizzbuzz_test():
+    assert fizzbuzz(12.5) == "fizz"
+    assert fizzbuzz(2)    == 27
+
+
+
+
+
+
+
+
+
+
+
