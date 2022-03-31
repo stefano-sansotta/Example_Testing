39fa1b10818d1af938c3f08acb158021f8654eda
stefano-sansotta <sansotta.stefano@gmail.com>
2022-03-31 10:33:26 +0200

    Correction

--- test_test.py
+++ test_test.py
@@ -7,8 +7,8 @@ def Add(a, b):
 
 def test_Add():
 #    assert abs(Add(0.1, 0.2)-0.3) < 0.00000000000000000001
-#    assert Add(0.1, 0.2) == approx(0.3)
-    assert Add(0.1, 0.2) == 0.3
+    assert Add(0.1, 0.2) == approx(0.3)
+#    assert Add(0.1, 0.2) == 0.3
     assert Add("Star", "wars") == "Starwars"
 
 
